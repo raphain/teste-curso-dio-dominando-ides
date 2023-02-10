ba96a325bf614360cb77c961534114c7fdc16a62
Raphain <raphain1@hotmail.com>
2023-02-10 19:49:34 -0300

    1

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -9,7 +9,7 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		//System.out.println("Teste");
 		Gato gato = new Gato();
-		Livro livros = new Livro();
+		//Livro livros = new Livro();
 		System.out.println(gato);
 		
 	}
