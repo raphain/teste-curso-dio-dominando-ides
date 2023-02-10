ea802a921828cd57cb63ef587fa2948abf59ea30
Raphain <raphain1@hotmail.com>
2023-02-10 19:32:10 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	
+
+	public static void main(String[] args) {
+		//System.out.println("Teste");
+		Gato gato = new Gato();
+		Livro livros = new Livro();
+		System.out.println(gato);
+		
+	}
+
+	public class Livro{
+		
+	}
+	
+}
+
